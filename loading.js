ab92656bae234418684e027ad191361227db07dc
DevCode-930 <kamranabbasidsl@gmail.com>
2025-04-08 23:00:10 +0500

    index.html

--- loading.js
+++ loading.js
@@ -7,4 +7,11 @@ function myFunction() {
 function showPage() {
   document.getElementById("loading").style.display = "none";
   document.getElementById("spinner").style.display = "block";
-}
+};
+const popup = document.getElementById("popup");
+const content = document.getElementById("content");
+const okButton = document.getElementById("okButton");
+okButton.addEventListener("click", function() {
+    popup.style.display = "none";
+    content.style.display = "block";
+});
